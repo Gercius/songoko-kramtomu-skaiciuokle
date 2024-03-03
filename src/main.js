87f9ade6d44f5dd1bf62b557115ca6f326dcc48d
gercius <gerciokaz@gmail.com>
2024-03-03 13:21:58 +0200

    add kamehame sound effects, refactor

--- src/main.js
+++ src/main.js
@@ -1,43 +1,46 @@
-const songokoKramtomaPrice = 0.03969;
+const inputElement = document.querySelector("#euros");
+const outputElement = document.querySelector(".output");
+const buttonElement = document.querySelector("button");
+const gumeHameWrapper = document.querySelector(".gume-hame-wrapper");
+
+buttonElement.addEventListener("click", (e) => {
+    const eur = parseFloat(inputElement.value);
+    outputElement.style.display = "block";
+
+    if (eur > Number.MAX_SAFE_INTEGER) {
+        outputElement.innerHTML = "Prašome įvesti mažesnį skaičių";
+        return;
+    } else if (eur < 0.03969) {
+        outputElement.innerHTML = `Prašome įvesti didesnį skaičių.<br>min. 0.03969<br>(1 kramtoma)`;
+        return;
+    }
+
+    buttonElement.style.display = "none";
+    inputElement.disabled = true;
+    const kramtomos = convertEurToKramtomos(eur);
+
+    setTimeout(() => {
+        outputElement.style.display = "block";
+        outputElement.innerHTML = `Jums išeina ${kramtomos} <span>Son Goko</span> kramtomos`;
+        gumeHameWrapper.style.display = "flex";
+    }, 100);
+
+    executeAnimations(kramtomos);
+});
 
 function convertEurToKramtomos(eur) {
-    console.log(Math.round(eur / songokoKramtomaPrice));
+    const songokoKramtomaPrice = 0.03969;
     return Math.round(eur / songokoKramtomaPrice);
 }
 
-function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-async function launchGumeHameHa(positions, animationsLength) {
-    for (let i = 0; i < positions.length; i++) {
-        let sound;
-
-        await sleep(animationsLength[i]);
-        positions[i].style.display = "block";
-
-        if (i > 0) positions[i - 1].style.display = "none";
-
-        if (i === 1) {
-            sound = new Audio("assets/kaio-ken.mp3");
-        } else if (i === 2) {
-            sound = new Audio("assets/ssj.mp3");
-        }
-
-        if (sound) sound.play();
-    }
-}
-
 async function executeAnimations(kramtomos) {
-    const gokuPositions = document.querySelectorAll(".goku-wrapper img");
-    const gokuPositionsLength = [500, 1500, 1500, 1500, 1500];
-
-    await launchGumeHameHa(gokuPositions, gokuPositionsLength);
+    await playGumeHameHa();
 
     const gumWrapper = document.querySelector(".gum-wrapper");
     const gumElement = document.querySelector(".gum");
     gumElement.style.display = "block";
 
+    // speed control of adding chewing gums/gumehameha
     const gumAdditionWait = 250 - kramtomos;
     const lowestWait = 50;
 
@@ -64,32 +67,29 @@ async function executeAnimations(kramtomos) {
     sound.pause();
 }
 
-const inputElement = document.querySelector("#euros");
-const outputElement = document.querySelector(".output");
-const buttonElement = document.querySelector("button");
-const gumeHameWrapper = document.querySelector(".gume-hame-wrapper");
+async function playGumeHameHa() {
+    const positions = document.querySelectorAll(".goku-wrapper img");
+    const positionsAnimationLength = [500, 1500, 1500, 1500, 3900];
+    const soundFiles = [
+        "",
+        "assets/kaio-ken.mp3",
+        "assets/ssj.mp3",
+        "assets/kamehame.mp3",
+        "assets/ha-long.mp3",
+    ];
 
-buttonElement.addEventListener("click", (e) => {
-    const eur = parseFloat(inputElement.value);
-    outputElement.style.display = "block";
+    for (let i = 0; i < positions.length; i++) {
+        await sleep(positionsAnimationLength[i]);
+        positions[i].style.display = "block";
+        if (i > 0) positions[i - 1].style.display = "none";
 
-    if (eur > Number.MAX_SAFE_INTEGER) {
-        outputElement.innerHTML = `Prašome įvesti mažesnį skaičių.`;
-        return;
-    } else if (eur < 0.03969) {
-        outputElement.innerHTML = `Prašome įvesti didesnį skaičių.<br> min. 0.03969 <br>(1 kramtoma)`;
-        return;
+        if (soundFiles[i]) {
+            const sound = new Audio(soundFiles[i]);
+            sound.play();
+        }
     }
+}
 
-    buttonElement.style.display = "none";
-    inputElement.disabled = true;
-    const kramtomos = convertEurToKramtomos(eur);
-
-    setTimeout(() => {
-        outputElement.style.display = "block";
-        outputElement.innerHTML = `Jums išeina ${kramtomos}  <span>Son Goko</span> kramtomos.`;
-        gumeHameWrapper.style.display = "flex";
-    }, 100);
-
-    executeAnimations(kramtomos);
-});
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
