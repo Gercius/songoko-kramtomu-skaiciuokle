c31cc23d5acedba50c91f55cfad0f8b9b2081155
gercius <gerciokaz@gmail.com>
2024-03-10 17:41:08 +0200

    refactor file structure

---
+++ src/js/audio.js
@@ -0,0 +1,15 @@
+export const audio = {};
+
+(function preloadSounds() {
+    const soundFiles = {
+        gumehameha: "../../assets/gumehameha.mp3",
+        kaioKen: "../../assets/kaio-ken.mp3",
+        SSJ: "../../assets/ssj.mp3",
+        kamehameha: "../../assets/kamehame.mp3",
+        haLong: "../../assets/ha-long.mp3",
+    };
+
+    for (const [key, value] of Object.entries(soundFiles)) {
+        audio[key] = new Audio(value);
+    }
+})();
